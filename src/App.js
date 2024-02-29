6e612a7977b7807edbb3bf28f6e278ad1f560222
TusharTummala <ttummala99@gmail.com>
2024-02-29 23:20:14 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './login';
 import Register from './register';
-import './App.css'; 
+import './navbar.css';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="d-flex-center min-vh-100">
-        <div className="card-custom shadow rounded-lg border-primary">
-          <div className="card-body">
-            <Navbar />
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-md-8">
-                  <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                  </Routes>
+      <div>
+        <Navbar />
+        <div className="d-flex-center min-vh-100">
+          <div className="card-custom shadow rounded-lg border-primary d-flex align-items-center justify-content-center">
+            <div className="card-body text-center content">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-8">
+                    <Routes>
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/register" element={<Register />} />
+                    </Routes>
+                  </div>
                 </div>
               </div>
             </div>
@@ -31,33 +34,24 @@ function App() {
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link to="/" className="navbar-brand">Your App Name</Link>
+       
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
           aria-expanded="false"
-          aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+       
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/register" className="nav-link">
-                Register
-              </Link>
-            </li>
-          </ul>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link to="/login" className="nav-item nav-link">Login</Link>
+            <Link to="/register" className="nav-item nav-link">Register</Link>
+          </div>
         </div>
       </div>
     </nav>
